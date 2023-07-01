84ea4585c6224b12809250ea7a08e3982a5f060a
Leonardo Avelino <leonardo.avelino16@hotmail.com>
2023-07-01 18:06:10 -0300

    fix: utils-automatic build

--- packages/utils-automatic/src/index.ts
+++ packages/utils-automatic/src/index.ts
@@ -2,4 +2,4 @@ console.log("utils");
 
 export const sub = (a: number, b: number) => a - b;
 
-export const sum = (a: number, b: number) => a + b;
+export const sum = (a: number, b: number) => a / b;
